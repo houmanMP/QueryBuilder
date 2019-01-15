fb563f9fddf137e2ba5954cddeb3881d3f7bba8f
houmanMP <43150207+houmanMP@users.noreply.github.com>
2019-01-15 13:58:10 -0500

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# QueryBuilder
-Created with CodeSandbox
---
+++ src/ConditionBuilder.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+const ConditionBuilder = ({
+  handleChange,
+  properties: { parameter, operator, value } = {}
+}) => (
+  <div>
+    <select
+      value={parameter}
+      onChange={({ target: { value } }) =>
+        handleChange({
+          parameter: value
+        })
+      }
+      required
+    >
+      {!parameter && <option>Select a condition</option>}
+      <option value={"companySize"}>Company Size</option>
+    </select>
+    {parameter && (
+      <select
+        value={operator}
+        onChange={({ target: { value } }) =>
+          handleChange({
+            operator: value
+          })
+        }
+        required
+      >
+        {!operator && <option>operator</option>}
+        <option value={"is"}>is</option>
+        <option value={"isNot"}>is not</option>
+      </select>
+    )}
+    {parameter && (
+      <select
+        value={value}
+        onChange={({ target: { value } }) =>
+          handleChange({
+            value
+          })
+        }
+        required
+      >
+        {!value && <option>value</option>}
+        <option value={"small"}>small</option>
+        <option value={"medium"}>medium</option>
+        <option value={"large"}>large</option>
+      </select>
+    )}
+  </div>
+);
+
+export default ConditionBuilder;
